973efe6b4133004ff90d681aac8165c8d8905c75
toran billups <toranb@gmail.com>
2018-08-04 08:32:21 -0500

    FIN: completed uber workshop web app

--- app/components/ui-layout/component.js
+++ app/components/ui-layout/component.js
@@ -1,5 +1,17 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['ui-layout']
+  classNames: ['ui-layout'],
+  actions: {
+    stop() {
+      this.set('riders', []);
+    },
+    start() {
+      this.set('riders', [
+        {lat: 41.5911, lng: -93.611, name: 'Toran'},
+        {lat: 41.5822, lng: -93.608, name: 'Michelle'},
+        {lat: 41.5879, lng: -93.613, name: 'Tim'}
+      ]);
+    }
+  }
 });
